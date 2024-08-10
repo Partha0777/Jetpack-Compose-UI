aa71aa859be536e6a138057984ba84fae8eecb57
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-08-10 22:46:25 +0530

    Added sample side navigation for learn the side navigation in the jetpack and need to implement the UI.

--- app/src/main/java/com/curiozing/jetpackcomposeui/MainActivity.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/MainActivity.kt
@@ -3,15 +3,26 @@ package com.curiozing.jetpackcomposeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.curiozing.jetpackcomposeui.ui.theme.JetpackComposeUITheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +41,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Home(){
-    Column {
-        Text(text = "Hello")
-    }
+fun Home() {
+    SideNavigationMenu()
+
+}
+
+@Composable
+fun SideNavigationMenu() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val localConfiguration = LocalConfiguration
+    val  screenWidth = localConfiguration.current.screenWidthDp
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Surface(color = Color.White, modifier = Modifier.fillMaxHeight().width(width = screenWidth.minus(screenWidth.div(4)).dp)) {
+                Column {
+                    Text(text = "Profile")
+                    Text(text = "About")
+                }
+            }
+
+        }, content = {
+            Column {
+                Text(text = "Hello")
+                Text(text = "Open Drawer", modifier = Modifier.clickable(onClick = {
+                    scope.launch {
+                        drawerState.open()
+                    }
+                }))
+            }
+        })
 }
 
 @Preview
