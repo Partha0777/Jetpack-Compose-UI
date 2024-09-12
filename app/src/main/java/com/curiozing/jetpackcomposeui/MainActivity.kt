04da2768186053b3591dda397c8b2410022105b0
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-12 21:08:47 +0530

    Created separate object for mock data.

--- app/src/main/java/com/curiozing/jetpackcomposeui/MainActivity.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/MainActivity.kt
@@ -72,6 +72,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.curiozing.jetpackcomposeui.ui.theme.JetpackComposeUITheme
+import com.curiozing.jetpackcomposeui.utils.MockData
 import kotlinx.coroutines.launch
 
 
@@ -671,35 +672,7 @@ fun TopBar() {
 @Composable
 fun RecommendedCategory() {
 
-    val recommendedCategory = listOf(
-        Category(1, "Italian", "https://onthewood.com/wp-content/uploads/2024/03/Italian1.jpg"),
-        Category(
-            1,
-            "Chinese",
-            "https://images-cdn.welcomesoftware.com/Zz0zMDM2ZWM5NmQ5YjAxMWViODcwYmI5NWUzYmNlYzM0NA==/Zz01NTg2OGYyMmQ4MmYxMWViOGM4NjRkNDA4MzFmNzQ4OA%3D%3D.jpg?width=1366"
-        ),
-        Category(
-            1,
-            "Indian",
-            "https://images.moneycontrol.com/static-mcnews/2023/10/pexels-anil-sharma-10580198-770x433.jpg"
-        ),
-        Category(
-            1,
-            "Mexican",
-            "https://curlytales.com/wp-content/uploads/2019/10/Best-Mexican-restaurants-in-the-world.jpg"
-        ),
-        Category(
-            1,
-            "Japanese",
-            "https://www.tastingtable.com/img/gallery/20-japanese-dishes-you-need-to-try-at-least-once/l-intro-1664219638.jpg"
-        ),
-        Category(
-            1, "French", "https://media.tacdn.com/media/attractions-content--1x-1/12/60/c1/ed.jpg"
-        ),
-        Category(
-            1, "Thai", "https://kampatour.com/pic/blog/a5d47844-5e3b-4172-94c8-ef5027e47ff8.jpg"
-        ),
-    )
+
     Column {
         Text(
             text = "Recommended Category",
@@ -708,7 +681,7 @@ fun RecommendedCategory() {
             modifier = Modifier.padding(horizontal = 20.dp)
         )
         LazyRow(content = {
-            recommendedCategory.take(4).forEach { category ->
+            MockData.recommendedCategory.take(4).forEach { category ->
                 item {
                     RecommendedCategoryItem(category)
                 }
