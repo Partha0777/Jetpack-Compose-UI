aa5e5dfbc5dc634ab3ccad15cf6f180116b32a93
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-12 21:09:58 +0530

    Created separate object for mock data.

--- app/src/main/java/com/curiozing/jetpackcomposeui/utils/MockData.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/utils/MockData.kt
@@ -1,6 +1,7 @@
 package com.curiozing.jetpackcomposeui.utils
 
 import com.curiozing.jetpackcomposeui.Category
+import com.curiozing.jetpackcomposeui.Orders
 
 object MockData {
     val recommendedCategory = listOf(
@@ -32,5 +33,33 @@ object MockData {
             1, "Thai", "https://kampatour.com/pic/blog/a5d47844-5e3b-4172-94c8-ef5027e47ff8.jpg"
         ),
     )
+
+    var recentOrders = listOf(
+        Orders(
+            orderId = 1,
+            products = "Chicken Pizza x 2",
+            totalPrice = 6.2,
+            outLetName = "Pizza Hut",
+            rating = 4.3,
+            image = "https://www.sargento.com/assets/Uploads/Recipe/Image/TuscanChikPizza_010__FillWzExNzAsNTgzXQ.jpg",
+        ),
+        Orders(
+            orderId = 2,
+            products = "Paneer Kadai",
+            totalPrice = 2.4,
+            outLetName = "Bind Resto",
+            rating = 4.5,
+            image = "https://dinedelicious.in/wp-content/uploads/2021/10/Kadai-Paneer-Recipe-6.jpg",
+        ),
+        Orders(
+            orderId = 3,
+            products = "Cold Brew Black",
+            totalPrice = 5.2,
+            outLetName = "Starbucks",
+            rating = 4.6,
+            image = "https://www.simplyrecipes.com/thmb/t9ZeQC3Nb2YUoQTnxUJrjJbnKEA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Cold-Brew-Coffee-LEAD-16-428691bcdd594281b2f5dc6dbc8235e4.jpg",
+        ),
+        )
+
 
 }
