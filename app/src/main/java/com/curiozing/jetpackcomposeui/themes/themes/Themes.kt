49a17b3afbba8d130a6d4c4622bc515b84eddd36
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-16 21:32:55 +0530

    Added theme list.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/themes/Themes.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/themes/Themes.kt
@@ -6,13 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -39,9 +43,9 @@ fun ThemeList(navigateToTheme: (page: String) -> Unit) {
             .fillMaxSize()
             .padding(horizontal = 20.dp, vertical = 20.dp)
     ) {
-        Text(text = "Theme 1", Modifier.clickable {
-            navigateToTheme.invoke("Theme1")
-        })
+        Text(text = "Themes", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+      //  navigateToTheme.invoke( "Theme1")
     }
 }
 
