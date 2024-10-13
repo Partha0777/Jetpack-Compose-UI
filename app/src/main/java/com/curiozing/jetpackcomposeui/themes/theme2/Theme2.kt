a9a0c5334c008a63918e8ef77d629e17cf7d770e
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-13 23:01:37 +0530

    Removed unwanted logs.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -115,10 +115,6 @@ fun Theme2() {
                         modifier = Modifier
                             .layout { measurable, constraints ->
                                 val placeable = measurable.measure(constraints)
-                                Log.d(
-                                    "Height --> screenHeight",
-                                    "${placeable.height} $screenHeight"
-                                )
                                 layout(placeable.width, placeable.height - (screenHeight + 100)) {
                                     placeable.placeRelative(0, -(screenHeight + 50)) // Shift up
                                 }
