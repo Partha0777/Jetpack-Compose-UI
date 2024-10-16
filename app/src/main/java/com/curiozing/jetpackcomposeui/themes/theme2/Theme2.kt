c6a1f2330660c0977664a9e5be82d7fc13448fe9
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-16 23:21:06 +0530

    Removed half circle rotating from theme 2.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -1,33 +1,8 @@
 package com.curiozing.jetpackcomposeui.themes.theme2
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.dp
-import kotlin.math.cos
-import kotlin.math.sin
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import kotlin.math.atan2
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Icon
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.platform.LocalConfiguration
 
 
 @Composable
