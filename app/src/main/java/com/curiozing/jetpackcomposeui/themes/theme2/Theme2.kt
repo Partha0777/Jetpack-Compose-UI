a31f61c4d37707db991b6fb85f696fc7846739d5
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-17 23:16:50 +0530

    Added Gradient background in the theme 2.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -5,12 +5,21 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
 
 
 @Composable
 fun Theme2() {
     Surface {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().background(brush = Brush.linearGradient(
+            listOf(
+                Color(0xFF155761),
+                Color(0xFF114c57),
+                Color(0xFF0e3c4c)
+            )
+        ))) {
             Box {
             }
         }
