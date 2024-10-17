c109131c80f2c4d64c54219bf11a7ff48dae9825
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-17 23:31:36 +0530

    Made theme 2 view to full view with gradient bg.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -1,5 +1,7 @@
 package com.curiozing.jetpackcomposeui.themes.theme2
 
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -7,12 +9,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 
 @Composable
 fun Theme2() {
     Surface {
+        val context = LocalContext.current
+        val window = (context as? ComponentActivity)?.window
+        WindowCompat.setDecorFitsSystemWindows(window!!, false)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
         Column(modifier = Modifier.fillMaxSize().background(brush = Brush.linearGradient(
             listOf(
                 Color(0xFF155761),
