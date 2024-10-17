3d7865018c70c4facc0b7548f397f92e908cc4c2
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-17 23:03:29 +0530

    Added full size column in theme 2.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -1,14 +1,16 @@
 package com.curiozing.jetpackcomposeui.themes.theme2
 
+import androidx.compose.foundation.background
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
 
 
 @Composable
 fun Theme2() {
     Surface {
-        Column {
+        Column(modifier = Modifier.fillMaxSize()) {
             Box {
             }
         }
