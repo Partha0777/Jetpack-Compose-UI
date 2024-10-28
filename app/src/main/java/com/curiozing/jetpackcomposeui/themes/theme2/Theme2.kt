661caa87f43c17a2707804f7fa9ed1c0777bdc30
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-28 21:34:37 +0530

    Added simple UI for theme2.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -1,18 +1,28 @@
 package com.curiozing.jetpackcomposeui.themes.theme2
 
+import android.graphics.drawable.PaintDrawable
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.curiozing.jetpackcomposeui.R
 
 
 @Composable
@@ -28,6 +38,8 @@ fun Theme2() {
         )
 
         Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
                 .background(
@@ -39,9 +51,26 @@ fun Theme2() {
                         )
                     )
                 )
+
+
         ) {
-            Box {
+            Box(modifier = Modifier .padding(horizontal = 50.dp, vertical = 50.dp)) {
+                Image(painter = painterResource(id = R.drawable.grocery_lading_image), contentDescription ="")
             }
+            Text(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                text = "Discover the best products",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 32.sp,
+                lineHeight = 40.sp,
+                color = Color.White)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = "FreshBox is an online groceries store that provides quality cooking ingredients at affordable prices.",
+                fontSize = 16.sp,
+                lineHeight = 20.sp,
+                color = Color.White)
         }
     }
 }
