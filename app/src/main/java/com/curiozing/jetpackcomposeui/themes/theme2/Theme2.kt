678dfaf3bec9a3a9069cd5edf7dee6c1c62f3f4b
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-30 21:42:59 +0530

    Code cleanup has been done.

--- app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
+++ app/src/main/java/com/curiozing/jetpackcomposeui/themes/theme2/Theme2.kt
@@ -1,6 +1,5 @@
 package com.curiozing.jetpackcomposeui.themes.theme2
 
-import android.graphics.drawable.PaintDrawable
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
@@ -18,17 +17,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.curiozing.jetpackcomposeui.R
 
 
